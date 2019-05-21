74ed3d563d07908a8a3866fdd90c34a03e7972c4
Mario Seeli <mario.seeli@dectris.com>
2019-05-21 18:22:27 +0200

    lolin wlan example

--- lolin_oled/main.py
+++ lolin_oled/main.py
@@ -1,1 +1,13 @@
-import oled
+from oled import oled
+
+import wlan
+
+def display_ip():
+    ip = wlan.ip
+    oled.text("IP:", 0, 0)
+    oled.text("{:>3}.{:>3}.".format(*ip.split(".")[:2]), 0, 10)
+    oled.text("{:>3}.{:>3}".format(*ip.split(".")[2:]), 0, 20)
+
+oled.fill(0)
+display_ip()
+oled.show()
