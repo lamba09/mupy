16897f2495de2f547f90d6e4b9de0072428d7589
Mario Seeli <mario.seeli@dectris.com>
2019-06-04 11:44:59 +0200

    esp8266 webrepl example

---
+++ esp8266_webrepl/main.py
@@ -0,0 +1,22 @@
+import time
+from machine import Pin
+led = Pin(2, Pin.OUT)  # turns on the LED
+
+time.sleep(0.5)
+led.on()
+time.sleep(0.5)
+led.off()
+
+import wlan
+import time
+
+time.sleep(0.5)
+led.on()
+time.sleep(0.5)
+led.off()
+
+import webrepl
+webrepl.start()
+
+time.sleep(0.5)
+led.on()
