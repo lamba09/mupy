e1ec9ef771e2bcc6ccdc967f77cf30498eb81810
Mario Seeli <marioseeli1986@hotmail.com>
2022-05-26 18:44:55 +0200

    fotobox v2

--- projects/fotobox/main.py
+++ projects/fotobox/main.py
@@ -20,53 +20,37 @@ def is_pressed(button):
     return button.value() == 0
 
 
-def cancel_request():
-    cancel_buttons = map(is_pressed, [RemoteTrigger, StopButton, BlackButton, GreenButton])
-    if any(cancel_buttons):
-        return True
-    return False
+def printing_active():
+    active = time.time() < last_print_time + 55
+    RedLED.value(active)
+    return active
 
+def test_image_enabled():
+    return is_pressed(Switch)
+
+def cancel_current_print():
+    time.sleep(0.5)
+    cancel_print()
+    time.sleep(0.5)
+    cancel_print()
+    time.sleep(0.5)
+    cancel_print()
+
+last_print_time = time.time() - 100
 
 def handle_printing():
-    time.sleep(0.5)
-    if not is_pressed(Switch):
-        cancel_print()
-        time.sleep(0.5)
-        cancel_print()
-        time.sleep(0.5)
-        cancel_print()
-    else:
-        RedLED.high()
-        cancelled = False
-        for i in range(50):
-            if cancel_request():
-                cancel_print()
-                time.sleep(0.2)
-                cancel_print()
-                cancelled = True
-                break
-            time.sleep(0.1)
-        if not cancelled:
-            time.sleep(50)
-        RedLED.low()
-    time.sleep(5)
-
-
-def timer(seconds):
-    timer_cancelled = False
-    for i in range(1, 10*int(seconds)+1):
-        if is_pressed(StopButton):
-            timer_cancelled = True
-            break
+    global last_print_time
+    if printing_active() and test_image_enabled():
+        cancel_current_print()
+        time.sleep(5)
+        return
+    if not printing_active():
+        last_print_time = time.time()
+    if test_image_enabled():
+        time.sleep(5)
+        return
+    while printing_active():
         time.sleep(0.1)
-        if i % 10 == 0:
-            StartStopLED.high()
-        elif (i + 3) % 10 == 0:
-            StartStopLED.low()
-    if not timer_cancelled:
-        trigger()
-        handle_printing()
-    StartStopLED.low()
 
 RED = LED(1)
 GREEN = LED(2)
@@ -77,7 +61,7 @@ uart = UART(1, 38400)
 
 # CONFIG  = b'\x0e$\x00\x10\x00\x13\x88\x00\x00\x00\x00\x00\\\x84 '
 CONFIG = b'\x06$\x00\x10\x00\x4e\x08\x00\x00\x00\x00\x00\xab\x68 '
-            
+
 def trigger():
     uart.write(b'\x02t ')
 
@@ -93,42 +77,45 @@ def ack():
 BLUE.on()
 RedLED.high()
 
+# wait for ready software
 while True:
     if uart.any():
         data = uart.read()
         if b'\x02i ' in data:
             connect()
             break
-    if is_pressed(RemoteTrigger):
-        break
+    # if is_pressed(RemoteTrigger):
+    #     break
     time.sleep(0.2)
 
 BLUE.off()
 YELLOW.on()
 GreenLED.high()
 
+# configure
 while True:
     if uart.any():
         data = uart.read()
         if b'\x02? ' in data:
             uart.write(CONFIG)
             break
-    if is_pressed(RemoteTrigger):
-        break
+    # if is_pressed(RemoteTrigger):
+    #     break
     time.sleep(0.2)
 
 YELLOW.off()
 RED.on()
 RedLED.low()
 
+# ackknowledge config
 while True:
     if uart.any() >= 64:
         data = uart.read(64)
         if CONFIG in data:
             ack()
             break
-    if is_pressed(RemoteTrigger):
-        break
+    # if is_pressed(RemoteTrigger):
+    #     break
     time.sleep(0.2)
 
 RED.off()
@@ -142,13 +129,7 @@ while True:
         trigger()
         GreenLED.low()
         handle_printing()
-    if is_pressed(StartButton):
-        StartStopLED.low()
+    while printing_active() and not test_image_enabled():
         GreenLED.low()
-        timer(seconds=5)
-    if is_pressed(GreenButton):
-        StartStopLED.low()
-        GreenLED.low()
-        timer(seconds=3)
-
+        time.sleep(0.1)
     time.sleep(0.05)
